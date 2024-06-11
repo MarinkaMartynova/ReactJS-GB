9b79099926e1c3659d568dd2e1cad8412c957b86
Marina <saramsok@gmail.com>
2024-06-12 00:37:15 +0400

    create homework 2

---
+++ homework-app-2/src/components/commentsList.js
@@ -0,0 +1,40 @@
+// Задача:
+// Создать компонент CommentsList, который отображает список комментариев. Каждый комментарий должен иметь кнопку для его удаления. При нажатии на кнопку комментарий должен удаляться из списка.
+
+// Можно использовать заготовку:
+
+// const [comments, setComments] = useState([
+// { id: 1, text: "Это первый комментарий" },
+// { id: 2, text: "Это второй комментарий" },
+// { id: 3, text: "Это третий комментарий" }
+// ]);
+
+import React, { useState } from "react";
+
+const CommentsList = () => {
+  const [comments, setComments] = useState([
+    { id: 1, text: "Это первый комментарий" },
+    { id: 2, text: "Это второй комментарий" },
+    { id: 3, text: "Это третий комментарий" },
+  ]);
+
+  const deleteComment = (id) => {
+    setComments(comments.filter((comment) => comment.id !== id));
+  };
+
+  return (
+    <div>
+      <h2>Комментарии</h2>
+      <ul>
+        {comments.map((comment) => (
+          <li key={comment.id}>
+            {comment.text}
+            <button onClick={() => deleteComment(comment.id)}>Удалить</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default CommentsList;
